c274b38f653887c6f956d4136b52b5cd075ba3de
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-02-24 22:59:39 -0300

    Exercicios 1 e 2

---
+++ FUNDAMENTOS/BLOCO_4/DIA_1/scripts.js
@@ -0,0 +1,31 @@
+let a = 5;
+let b = 4;
+
+//Programa 1
+let adicao = a + b;
+console.log (adicao);
+
+//Programa 2
+let subtracao = a - b;
+console.log (subtracao);
+
+//Programa 3
+let multiplicacao = a * b;
+console.log (multiplicacao);
+
+//Programa 4
+let divisao = a / b;
+console.log (divisao);
+
+//Programa 5
+let modulo = a % b;
+console.log (modulo);
+
+//Programa 6
+
+if (a > b) {
+  console.log (a)
+}
+else {
+  console.log (b)
+}
