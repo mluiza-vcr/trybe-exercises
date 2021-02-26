b71d4f289e3141c16502dbdf9e407e93fd226d7a
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-02-25 23:17:59 -0300

    Exercício 2

--- FUNDAMENTOS/BLOCO_4/DIA_2/scripts.js
+++ FUNDAMENTOS/BLOCO_4/DIA_2/scripts.js
@@ -7,3 +7,13 @@ for (index = 0; index < numbers.length; index ++){
   console.log (numbers[index]);
 }
 
+//EXERCÍCIO 2
+//Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (index = 0; index < numbers.length; index ++){
+  soma = soma + numbers[index]
+}
+console.log (soma)
