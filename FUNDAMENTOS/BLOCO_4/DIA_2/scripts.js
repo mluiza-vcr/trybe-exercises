f8d39fbe311068fc5166779280c6144540a87e31
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-02-26 03:30:24 -0300

    Exercício 9

--- FUNDAMENTOS/BLOCO_4/DIA_2/scripts.js
+++ FUNDAMENTOS/BLOCO_4/DIA_2/scripts.js
@@ -97,3 +97,14 @@ for (let index = 0; index < 25; index ++){
 }
 
 console.log(listaNova)
+
+//EXERCÍCIO 9
+//Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+let resultadoDaDivisao = []
+
+for (let indice = 0; indice < listaNova.length; indice++){
+  resultadoDaDivisao.push(listaNova[indice]/2)
+}
+
+console.log(resultadoDaDivisao)
