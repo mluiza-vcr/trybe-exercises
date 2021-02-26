6fea28e0da154afaaeafea0869a941e7e202fbf9
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-02-25 23:28:03 -0300

    Exercícios 3 e 4

--- FUNDAMENTOS/BLOCO_4/DIA_2/scripts.js
+++ FUNDAMENTOS/BLOCO_4/DIA_2/scripts.js
@@ -17,3 +17,25 @@ for (index = 0; index < numbers.length; index ++){
   soma = soma + numbers[index]
 }
 console.log (soma)
+
+//EXERCÍCIO 3
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+//A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+
+let media = 0
+
+for (index = 0; index < numbers.length; index ++){
+  media = (soma / numbers.length)
+}
+console.log (media)
+
+//EXERCÍCIO 4
+//Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: 
+//"valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+if (media > 20) {
+  console.log ("valor maior que 20")
+}
+else {
+  console.log ("valor menor ou igual a 20")
+}
