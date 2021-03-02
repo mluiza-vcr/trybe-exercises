c00acfa0f94bda8cd164332554d22e88005e7424
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-03-02 04:11:53 -0300

    Exercício 3

--- FUNDAMENTOS/BLOCO_4/DIA_4/parteII.js
+++ FUNDAMENTOS/BLOCO_4/DIA_4/parteII.js
@@ -31,10 +31,26 @@ function retornaIndiceDoMaior (array) {
   let maior = 0;
   for (let index = 0; index < array.length; index += 1) {
     if (array[index] > maior){
-      maior = indexOf(array[index])
+      maior = array[index]
     }
   }
-  console.log(maior)
+  console.log(array.indexOf(maior))
 }
 
 retornaIndiceDoMaior ([2, 3, 6, 7, 10, 1]);
+
+// 3. Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+//Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
+//Valor esperado no retorno da função: 6 .
+
+function retornaIndiceDoMenor (array) {
+  let menor = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < menor){
+      menor = array[index]
+    }
+  }
+  console.log (array.indexOf(menor))
+}
+
+retornaIndiceDoMenor ([2, 4, 6, 7, 10, 0, -3])
