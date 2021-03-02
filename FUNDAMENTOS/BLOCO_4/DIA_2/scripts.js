93671d7186e111599f7d80c0bb156877419d215b
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-03-02 02:23:39 -0300

    Alterações

--- FUNDAMENTOS/BLOCO_4/DIA_2/scripts.js
+++ FUNDAMENTOS/BLOCO_4/DIA_2/scripts.js
@@ -2,21 +2,27 @@
 //Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log()
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let xis = 0;
 
 for (index = 0; index < numbers.length; index ++){
   console.log (numbers[index]);
+  xis = numbers[index]
 }
 
 //EXERCÍCIO 2
 //Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+function ex2(){
+  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+  let soma = 0;
 
-for (index = 0; index < numbers.length; index ++){
-  soma = soma + numbers[index]
+  for (index = 0; index < numbers.length; index ++){
+    soma = soma + numbers[index]
+  }
+    console.log (soma)
 }
-console.log (soma)
+ex2()
+
 
 //EXERCÍCIO 3
 //Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
@@ -43,7 +49,7 @@ else {
 //EXERCÍCIO 5
 //Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let maior = numbers[0]
 
 for (index = 0; index < numbers.length; index ++){
@@ -57,7 +63,7 @@ console.log (maior)
 //EXERCÍCIO 6 
 //Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let quantidadeDeValoresImpares = 0
 
 for (index = 0; index < numbers.length; index ++){
@@ -76,7 +82,7 @@ else {
 //EXERCÍCIO 7
 //Utilizando for, descubra qual o menor valor contido no array e imprima-o;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let menor = numbers[0]
 
 for (index = 0; index < numbers.length; index ++){
