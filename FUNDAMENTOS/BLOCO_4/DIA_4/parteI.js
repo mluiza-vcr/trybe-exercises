9697b31b96dba899305d28f3909d66e882127606
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-03-02 02:54:09 -0300

    Exercício 2

--- FUNDAMENTOS/BLOCO_4/DIA_4/parteI.js
+++ FUNDAMENTOS/BLOCO_4/DIA_4/parteI.js
@@ -1,4 +1,4 @@
-//Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+//1. Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 //Valor esperado no console: Bem-vinda, Margarida
 
 let info = {
@@ -8,3 +8,9 @@ let info = {
 };
 
 console.log("Bem-vinda, " + info.personagem)
+
+//2. Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console.
+
+info.recorrente = "Sim";
+
+console.log (info)
