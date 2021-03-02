9598eed6aa4730855dc9e2b8691499e21d775664
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-03-02 03:20:01 -0300

    Exercício 5

--- FUNDAMENTOS/BLOCO_4/DIA_4/parteI.js
+++ FUNDAMENTOS/BLOCO_4/DIA_4/parteI.js
@@ -24,3 +24,27 @@ for (let key in info){
 for (let key in info){
 console.log(info[key])
 }
+
+//5. Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 
+//"Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim"
+}
+
+console.log ("outro exercício")
+
+for (let key in info){
+  for (let key in info2){
+  }
+    if (info[key] == info2[key]){
+    console.log ("Ambos recorrentes") 
+    }
+    else {
+    console.log (info[key] + " e " + info2[key])
+  }
+}
+
