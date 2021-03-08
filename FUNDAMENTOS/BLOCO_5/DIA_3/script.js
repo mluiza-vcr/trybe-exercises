39bf5a713d5172d326741906f74a8496647f119c
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-03-08 13:34:53 -0300

    Exercício 10

--- FUNDAMENTOS/BLOCO_5/DIA_3/script.js
+++ FUNDAMENTOS/BLOCO_5/DIA_3/script.js
@@ -171,12 +171,21 @@ function setTaskClass() {
 
 setTaskClass();
 
-function setDayColor () {
+function setDayColor() {
+  let selectedTask = document.getElementsByClassName('task selected');
   let days = document.querySelector('#days');
-  let selectedTask = document.querySelector ('.task selected');
   let taskDiv = document.querySelector('.task');
+  let taskColor = taskDiv.style.backgroundColor;
+  
+  days.addEventListener('click', function(event){
+    let eventTargetColor = event.target.style.color;
+    if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+      let color = selectedTask[0].style.backgroundColor;
+      event.target.style.color = color;
+    } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+      event.target.style.color = 'rgb(119,119,119)';
+    }
+  });
+};
 
-  days.addEventListener('click', function () {
-    
-  })
-}
+setDayColor();
