620edb1df0b2ea4f49f44befacc246f96066553e
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-03-09 00:01:34 -0300

    Iniciando exercícios Web Storage

---
+++ FUNDAMENTOS/BLOCO_5/DIA_4/script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+
+  function setBackgroundColor (color) {
+    backgroundColor = document.body.style.backgroundColor
+    localStorage.setItem("backgroundColor", color)
+  }
+
+
+
+
+
+
+
+}
