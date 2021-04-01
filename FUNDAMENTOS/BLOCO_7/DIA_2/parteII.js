eb84d347af05ffe061f282d6ff6bf3b3ca28db89
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-04-01 17:03:49 -0300

    parte II - exercícios 5 a 8

--- FUNDAMENTOS/BLOCO_7/DIA_2/parteII.js
+++ FUNDAMENTOS/BLOCO_7/DIA_2/parteII.js
@@ -36,7 +36,7 @@ console.log(showKeys(lesson1));
 
 // 3. Crie uma função para mostrar o tamanho de um objeto.
 
-const sizeObj = (obj) =>  Object.keys(obj).length;
+const sizeObj = (obj) => Object.keys(obj).length;
 
 console.log(sizeObj(lesson1));
 
@@ -45,3 +45,43 @@ console.log(sizeObj(lesson1));
 const showValues = (obj) => Object.values(obj);
 
 console.log(showValues(lesson1));
+
+// 5. Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . 
+
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+
+console.log(allLessons);
+
+// 6. Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+const numberOfStudents = (obj) => {
+  const numberOfStudents1 = obj.lesson1.numeroEstudantes;
+  const numberOfStudents2 = obj.lesson2.numeroEstudantes;
+  const numberOfStudents3 = obj.lesson3.numeroEstudantes;
+
+  return numberOfStudents1 + numberOfStudents2 + numberOfStudents3;
+}
+
+console.log(numberOfStudents(allLessons));
+
+// 7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+
+const getValueByNumber = (obj, position) => Object.values(obj)[position];
+
+console.log(getValueByNumber(lesson1, 1))
+
+// 8. Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. 
+
+const verifyPair = (obj, key, value) => {
+  const arr = Object.entries(obj); //array pares valor-chave
+  let isEqual = false;
+  for(let index in arr) {
+    if (arr[index][0] === key && arr[index][1] === value) {
+      isEqual = true;
+    }
+  }
+  return isEqual;
+}
+
+console.log(verifyPair(lesson3, 'turno', 'noite'));
+
