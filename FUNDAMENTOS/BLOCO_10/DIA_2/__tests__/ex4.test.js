8e56b6a5c524373f6c15abedc5d36aa7ee7765d4
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-04-20 20:35:46 -0300

    Exercício 5 Jest assíncrono

--- FUNDAMENTOS/BLOCO_10/DIA_2/__tests__/ex4.test.js
+++ FUNDAMENTOS/BLOCO_10/DIA_2/__tests__/ex4.test.js
@@ -6,4 +6,5 @@ test('testa se repositórios estão na lista do resultado', async () => {
   expect.assertions(2);
   expect(response.includes('sd-01-week4-5-project-todo-list')).toBeTruthy();
   expect(response.includes('sd-01-week4-5-project-meme-generator')).toBeTruthy();
-})
+});
+
