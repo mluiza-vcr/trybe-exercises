28c69ee891ad112798a042b5a75acc9fd69c9b72
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-04-20 19:03:22 -0300

    Exercício 2 - Jest

--- FUNDAMENTOS/BLOCO_10/DIA_2/__tests__/ex2.test.js
+++ FUNDAMENTOS/BLOCO_10/DIA_2/__tests__/ex2.test.js
@@ -1,2 +1,18 @@
 const { TestScheduler } = require("jest");
-const  = require('../src/ex1');
+const { findUserById, getUserName } = require('../src/ex2');
+
+describe('testa resultados da função getUserName quando usuário é encontrado e quando não é', () => {
+  test('testa resultado da função quando usuário é encontrado', () => {
+    expect.assertions(1);
+    return getUserName(4).then((userName) => {
+      expect(userName).toBe('Mark');
+    });
+  });
+  test('testa resultado da função getUserName quando usuário não é encontrado', () => {
+    expect.assertions(1);
+    return getUserName(3).catch((response) => {
+      const object = { error: 'User with 3 not found.' }
+      expect(response).toEqual(object);
+    });
+  });
+});
