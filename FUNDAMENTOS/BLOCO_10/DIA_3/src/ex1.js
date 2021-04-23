e795868f7763cafaf0a3815bc6884bcb121fc246
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-04-22 22:56:17 -0300

    Exercício 4 mock

--- FUNDAMENTOS/BLOCO_10/DIA_3/src/ex1.js
+++ FUNDAMENTOS/BLOCO_10/DIA_3/src/ex1.js
@@ -2,4 +2,13 @@ const showRandomNumber = () => {
   return Math.floor(Math.random() * 101)
 };
 
-module.exports = { showRandomNumber };
+const showString = (str) => {
+  return str.toUpperCase();
+}
+const showFirstLetter = (str) => {
+    return str[0];
+}
+const concatStrings = (str1, str2) => {
+    return `${str1}${str2}`
+}
+module.exports = { showRandomNumber, showFirstLetter , showString , concatStrings };
