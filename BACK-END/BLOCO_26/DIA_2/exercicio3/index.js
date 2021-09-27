6443e20d1131cd612458ecfdb68357b2e91f10ae
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-09-27 19:23:57 -0300

    Reescreva o código do exercício anterior para que utilize async/await

---
+++ BACK-END/BLOCO_26/DIA_2/exercicio3/index.js
@@ -0,0 +1,27 @@
+function calculate(num1, num2, num3) {
+  return new Promise((resolve, reject) => {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof num3 !== 'number') {
+      return reject('Informe apenas números');
+    }
+    const result = (num1 + num2) * num3;
+    if (result < 50) {
+      return reject('Valor muito baixo');
+    }
+    resolve(result);
+  })
+}
+
+const a = Math.floor(Math.random() * 100 + 1);
+const b = Math.floor(Math.random() * 100 + 1);
+const c = Math.floor(Math.random() * 100 + 1);
+
+async function calculateAsync() {
+  try {
+    const result = await calculate(a, b, c)
+    console.log(result);
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+calculateAsync();
