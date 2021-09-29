dd402506bca74148cc92827c592aad9fe1af4a8a
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-09-28 21:12:44 -0300

    Adapte os testes adicionando stub ao módulo utilizado do fs , isolando assim o teste.

--- BACK-END/BLOCO_26/DIA_3/tests/exercicio4Test.js
+++ BACK-END/BLOCO_26/DIA_3/tests/exercicio4Test.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
+const sinon = require('sinon');
 const { expect } = require('chai');
 const writeMyFile = require('../exercicio4');
 
 describe('Chama a função "writeMyFile"', () => {
+  
+  before(() => {
+    sinon.stub(fs, 'writeFileSync')
+  });
+  after(() => {
+    fs.writeFileSync.restore();
+  });
+  
   describe('a resposta', () => {
     it('é uma string', () => {
       const resposta = writeMyFile('arquivo.txt', '#vqv conteúdo');
